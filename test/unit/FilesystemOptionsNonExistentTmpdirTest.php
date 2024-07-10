3fd0ccc609cb536e5426e6278303f486ccbbe363
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2024-07-10 14:22:59 +0200

    qa: add macos temp directory support
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/unit/FilesystemOptionsNonExistentTmpdirTest.php
+++ test/unit/FilesystemOptionsNonExistentTmpdirTest.php
@@ -10,15 +10,30 @@ use PHPUnit\Framework\TestCase;
 use function is_writable;
 use function sys_get_temp_dir;
 
+use const PHP_OS_FAMILY;
+
 final class FilesystemOptionsNonExistentTmpdirTest extends TestCase
 {
+    private const MACOS_FAMILY = 'Darwin';
+
     public function testWillNotWriteToSystemTempWhenCacheDirIsProvided(): void
     {
         if (is_writable(sys_get_temp_dir())) {
             self::markTestSkipped('Test has to be executed with a non existent TMPDIR');
         }
 
+        /**
+         * Due to the usage of `realpath` {@see FilesystemOptions::normalizeCacheDirectory()}, the directory is changed
+         * depending on the host system. As there might be developers using MacOS brew PHP to execute these tests, we
+         * allow MacOS `/private/tmp` as well.
+         */
+        $expectedTemporaryDirectory = match (PHP_OS_FAMILY) {
+            default => '/tmp',
+            self::MACOS_FAMILY => '/private/tmp',
+        };
+
         $option = new FilesystemOptions(['cacheDir' => '/./tmp']);
-        self::assertSame('/tmp', $option->getCacheDir());
+
+        self::assertSame($expectedTemporaryDirectory, $option->getCacheDir());
     }
 }
